d0a9d044954fc082f61bad1ef4f68620ac12fab7
blackwhitebear <60433367+sid4202@users.noreply.github.com>
2023-10-02 11:04:17 +0300

    Update tasks12-14.cpp

--- tasks12-14.cpp
+++ tasks12-14.cpp
@@ -22,7 +22,7 @@ int itc_find_str(string str1, string str2)
 {
     for (int i = 0; i < itc_len(str1) - itc_len(str2) + 1; i++)
     {
-        if (itc_slice_str(str1, i, i + itc_len(str2) - 1) == str2 + '\0')
+        if (itc_slice_str(str1, i, i + itc_len(str2) - 1) == str2)
         {
             return i;
         }
