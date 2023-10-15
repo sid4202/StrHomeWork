d228b2278e64338b6da7c3ce15b42edfc2423c2e
blackwhitebear <maxonleb5@gmail.com>
2023-10-15 15:39:36 +0300

    HomeWork

--- tasks12-14.cpp
+++ tasks12-14.cpp
@@ -36,28 +36,34 @@ int itc_find_str(string str1, string str2)
 
 string itc_three_str(string str1, string str2, string str3)
 {
-    string answer;
+    string answer = "";
+
+    if (itc_find_str(str1, str2) == -1)
+        return str1;
 
     while (itc_find_str(str1, str2) != -1)
     {
         int substr_pos = itc_find_str(str1, str2);
 
-        int str3_index = 0;
+        for (int i = 0; i < substr_pos; i++)
+        {
+            answer += str1[i];
+        }
 
+        answer += str3;
+
+        if (substr_pos + itc_len(str2) > itc_len(str1) - 1)
+            break;
+
+        str1 = itc_slice_str(str1, substr_pos + itc_len(str2), itc_len(str1) - 1);
+        cout << answer << " " << str1 << endl;
+    }
+
+    if (itc_find_str(str1, str2) == - 1)
+    {
         for (int i = 0; i < itc_len(str1); i++)
         {
-           if (i >= substr_pos and i <= substr_pos + itc_len(str2) - 1)
-           {
-               if (str3_index <= itc_len(str3) - 1)
-               {
-                   answer += str3[str3_index];
-                   str3_index++;
-               } else {
-                   answer += str1[i];
-               }
-           } else {
-               answer += str1[i];
-           }
+            answer += str1[i];
         }
     }
 
